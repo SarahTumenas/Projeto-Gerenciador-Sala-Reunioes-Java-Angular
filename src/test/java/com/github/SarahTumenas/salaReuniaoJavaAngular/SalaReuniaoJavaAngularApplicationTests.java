1f72fbd7c9cc488a7c262b69a102b93756516f1c
SarahTumenas <sarahtumenas@yahoo.com.br>
2022-01-04 13:31:02 -0300

    First Commit

---
+++ src/test/java/com/github/SarahTumenas/salaReuniaoJavaAngular/SalaReuniaoJavaAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.SarahTumenas.salaReuniaoJavaAngular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaReuniaoJavaAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
